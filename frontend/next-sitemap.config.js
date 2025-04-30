69b437c9883a86c3e84e4aa2a529b8b0d956ed9a
Dev Dhanuka <lohandanuka@gmail.com>
2025-04-30 12:05:48 +0530

    sitemap updated

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,12 @@
 // next-sitemap.config.js
 module.exports = {
-  siteUrl: 'https://www.sriadz.com', // your domain
-  generateRobotsTxt: true,           // also generate robots.txt
-  sitemapSize: 5000,                 // optional
+  siteUrl: 'https://www.sriadz.com',
+  generateRobotsTxt: true,
+  exclude: [
+    '/admin',
+    '/admin/*',
+    '/account',
+    '/account/*',
+    '/dashboard',
+  ],
 };
